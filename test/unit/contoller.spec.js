8106abdc98a0e4b8c8f5ea8085d415f0da96a002
Kyle Finley <kyle.finley@gmail.com>
2012-10-22 13:32:55 -0500

    Added a controller test to demonstrate testing -- resolved issue #26. Angular-brunch-seed doesn't use Global controllers, therefore the controller must be referenced using '$controller' and the string name of the controller, E.g. "MyCtrl2"

--- test/unit/contoller.spec.js
+++ test/unit/contoller.spec.js
@@ -4,9 +4,16 @@
 describe("controllers", function() {
   beforeEach(module("app.controllers"));
   describe("MyCtrl1", function() {
-    return it("should...'");
+    return it("should make scope testable", inject(function($rootScope, $controller) {
+      var ctrl, scope;
+      scope = $rootScope.$new();
+      ctrl = $controller("MyCtrl1", {
+        $scope: scope
+      });
+      return expect(scope.onePlusOne).toEqual(2);
+    }));
   });
   return describe("MyCtrl2", function() {
-    return it("should...'");
+    return it("should...");
   });
 });
